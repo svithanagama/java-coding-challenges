44273221c9751b8badeb47d3a70a5a1b04a531bf
Sanjaya Vithanagama <sanjaya.vithanagama@mlcinsurance.com.au>
2018-02-16 17:48:45 +1100

    Added basic project structure

---
+++ codility/src/main/java/me/sanjayav/codility/App.java
@@ -0,0 +1,13 @@
+package me.sanjayav.codility;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
