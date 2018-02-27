c5d4b3fa76c1d867967315540b0cf85937b0a733
Sanjaya Vithanagama <sanjaya.vithanagama@mlcinsurance.com.au>
2018-02-27 16:38:20 +1100

    Added the missing module.

---
+++ general/src/test/java/me/sanjayav/general/AppTest.java
@@ -0,0 +1,16 @@
+package me.sanjayav.general;
+
+import org.junit.jupiter.api.Assertions;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+    @org.junit.jupiter.api.Test
+    public void testApp()
+    {
+        Assertions.assertTrue(true);
+    }
+}
